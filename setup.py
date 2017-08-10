da75fa4f9e1d2b9a6f934a313b27c1a9db4af780
taylorsansom <taylor.sansom@twdb.texas.gov>
2017-08-10 08:09:33 -0500

    changed to version 0.1

--- setup.py
+++ setup.py
@@ -2,9 +2,13 @@ from setuptools import setup
 
 setup(
     name='tbtools',
-    version='1.0.0',
+    packages = ['tbtools'],
+    version='0.1',
     description='Tools for reading/writing files associated with the TxBLEND model',
     author='Taylor Sansom',
     author_email='taylor.sansom@twdb.texas.gov',
-    url='https://github.com/twdb/tbtools'
+    url='https://github.com/twdb/tbtools',
+    download_url = 'https://github.com/twdb/tbtools/archive/0.1.tar.gz',
+    keywords = ['TxBLEND'],
+    classifiers = [],
     )
