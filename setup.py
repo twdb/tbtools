a9dce681ac482eac5e0ebe3822872873949c2d13
taylorsansom <taylor.sansom@twdb.texas.gov>
2018-02-27 10:42:53 -0600

    added read txblend tide input file

--- setup.py
+++ setup.py
@@ -2,13 +2,13 @@ from setuptools import setup, find_packages
 
 setup(
     name='tbtools',
-    packages = find_packages(),
+    packages=find_packages(),
     version='0.5.13',
     description='Tools for reading/writing files associated with the TxBLEND model',
     author='Taylor Sansom',
     author_email='taylor.sansom@twdb.texas.gov',
     url='https://github.com/twdb/tbtools',
-    download_url = 'https://github.com/twdb/tbtools/archive/0.2.tar.gz',
-    keywords = ['TxBLEND'],
-    classifiers = [],
+    download_url='https://github.com/twdb/tbtools/archive/0.2.tar.gz',
+    keywords=['TxBLEND'],
+    classifiers=[],
     )
